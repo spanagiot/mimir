c410d42015ea26843d80dee15db5b4ebf6e5b41b
Bryan Boreham <bjboreham@gmail.com>
2024-02-29 14:46:02 +0000

    Log more info about symboltable rebuild.

--- pkg/ingester/ingester.go
+++ pkg/ingester/ingester.go
@@ -2572,7 +2572,7 @@ func (i *Ingester) createTSDB(userID string, walReplayConcurrency int) (*userTSD
 			localSeriesLimit: initialLocalLimit,
 		},
 	}
-	userDB.resetSymbolTable()
+	userDB.resetSymbolTable(i.logger)
 	userDB.triggerRecomputeOwnedSeries(recomputeOwnedSeriesReasonNewUser)
 
 	oooTW := i.limits.OutOfOrderTimeWindow(userID)
@@ -3091,7 +3091,7 @@ func (i *Ingester) compactBlocks(ctx context.Context, force bool, forcedCompacti
 		default:
 			reason = "regular"
 			err = userDB.Compact()
-			userDB.resetSymbolTable()
+			userDB.resetSymbolTable(i.logger)
 		}
 
 		if err != nil {
