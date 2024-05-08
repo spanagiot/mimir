a7574d3d0a65502383f9e33c6437d005e6baa48c
Yuri Nikolic <durica.nikolic@grafana.com>
2024-05-08 22:04:33 +0200

    Adding deadlineExceeded endpoint
    Signed-off-by: Yuri Nikolic <durica.nikolic@grafana.com>

--- pkg/ingester/ingester.go
+++ pkg/ingester/ingester.go
@@ -3818,6 +3818,15 @@ func (i *Ingester) push(ctx context.Context, req *mimirpb.WriteRequest) (*mimirp
 
 	level.Info(i.logger).Log("msg", "Push is being executed", "ingester", ingesterID, "currentTimeout", currentTimeout, "remainingTime", remainingTime, "requestID", requestID)
 
+	if deadlineExceedEnabled.Load() {
+		userID, err := tenant.TenantID(ctx)
+		if err != nil {
+			return nil, err
+		}
+		time.Sleep(5 * time.Second)
+		level.Error(i.logger).Log("msg", "slept for 5s and will continue", "user", userID, "ingester", i.cfg.IngesterRing.InstanceID)
+	}
+
 	err := i.PushToStorage(ctx, req)
 
 	cT, ok = ctx.Deadline()
